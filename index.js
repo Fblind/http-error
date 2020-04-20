1a6ba3628237707c8bcd909b53fedd15a8c568a8
Fblind <fblind@gmail.com>
2020-04-20 15:57:27 -0300

    Change statusCode name to be more verbose

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 'use strict'
 
-function HttpError (code, message, statusCodeOverride) {
+function HttpError (code, message, statusCode) {
   // eslint-disable-next-line no-proto
   this.constructor.prototype.__proto__ = Error.prototype
   Error.captureStackTrace(this, this.constructor)
   this.name = this.constructor.name
   this.message = message || ''
-  this.status = statusCodeOverride || 400
+  this.status = statusCode || 400
   this.code = code || ''
 }
 
