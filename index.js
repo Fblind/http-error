bbd7807fd0111c01bbedc6d293a179c3a1514fcb
Fblind <fblind@gmail.com>
2020-04-20 15:37:26 -0300

    Add linter with standard and initial example test

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-"use strict";
+'use strict'
 
-function HttpError(code, message, statusCodeOverride) {
-  this.constructor.prototype.__proto__ = Error.prototype;
-  Error.captureStackTrace(this, this.constructor);
-  this.name = this.constructor.name;
-  this.message = message || "";
-  this.status = statusCodeOverride || 400;
-  this.code = code || "";
+function HttpError (code, message, statusCodeOverride) {
+  // eslint-disable-next-line no-proto
+  this.constructor.prototype.__proto__ = Error.prototype
+  Error.captureStackTrace(this, this.constructor)
+  this.name = this.constructor.name
+  this.message = message || ''
+  this.status = statusCodeOverride || 400
+  this.code = code || ''
 }
 
-module.exports = HttpError;
+module.exports = HttpError
